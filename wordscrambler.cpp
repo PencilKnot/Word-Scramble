9afcac587baec7b393a2495407af2b5d43e36a54
PencilKnot <96088121+PencilKnot@users.noreply.github.com>
2022-02-10 15:59:41 -0500

    Added list of words and game dialogue
    List of 50 words (may add more later on).

--- wordscrambler.cpp
+++ wordscrambler.cpp
@@ -12,18 +12,46 @@ int random(int a, int b){
 }
 
 string scramble(string word){
-    int first=0, last=word.length();
-    for(int i=first; i<last; i++){
-        int rand=random(1, word.length());
-        char index=word[i];
-        word[i]=word[rand];
-        word[rand]=index;
+    int first = 0, last = word.length();
+    for(int i = first; i < last; i++){
+        int rand = random(1, word.length());
+        char index = word[i];
+        word[i] = word[rand];
+        word[rand] = index;
     }
+    return word;
 }
 
 int main(){
-    string word;
-    cin >> word;
-    string scrambled = scramble(scramble(scramble(scramble(word))));
-    cout << "Here is a scrambled word: " << scrambled;
+    std::string wordlist[50]={"marine", "dimension", "equinox", "robot", "infinite", "dragon", "unity", "judgment", "canadian", "forestry",
+                              "classroom", "rainbow", "hope", "shift", "quest", "elite", "achieve", "loyalty", "legendary", "word",
+                              "goat", "leader", "avenue", "straw", "glue", "offense", "dine", "coal", "minute", "socialist",
+                              "judicial", "gradual", "throne", "hook", "graze", "sermon", "still", "pie", "exemption", "worth",
+                              "queue", "record", "crackpot", "departure", "pawn", "corn", "publisher", "degree", "air", "crime"};
+
+    bool play = true;
+    while(play){
+        string word = wordlist[random(0, 49)];
+        string next, answer;
+        bool isCorrect = false;
+        string scrambled = scramble(scramble(scramble(scramble(word))));
+        cout << "Unscramble the following word: ";
+        cout << scrambled << "\n";
+        cin >> answer;
+        while(!isCorrect){
+            if(answer == word){
+                cout << "That's the right word!\n";
+                isCorrect = true;
+            }
+            else{
+                cout << "That's incorrect. Try again!\n";
+                cin >> answer;
+            }
+        }
+        
+        cout << "Play again? Type yes to continue.\n";
+        cin >> next;
+        if(next != "yes") play = false;
+    }
+    
 }
