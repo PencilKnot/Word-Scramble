6bcd128e8d637fe55bc92737f0f9c97c20d155ea
PencilKnot <96088121+PencilKnot@users.noreply.github.com>
2022-02-10 16:51:06 -0500

    Added game title, guess counter
    Also added some useful comments and change a few variables to constant

--- wordscrambler.cpp
+++ wordscrambler.cpp
@@ -4,14 +4,14 @@
 using namespace std;
 
 
-int random(int a, int b){
+int random(int a, int b){ //generates random number between a and b
     srand((unsigned) time(NULL));
     
     int random = a + (rand() % b - 1);
     return random;
 }
 
-string scramble(string word){
+string scramble(string word){ //scrambles given string
     int first = 0, last = word.length();
     for(int i = first; i < last; i++){
         int rand = random(1, word.length());
@@ -23,20 +23,24 @@ string scramble(string word){
 }
 
 int main(){
-    std::string wordlist[50]={"marine", "dimension", "equinox", "robot", "infinite", "dragon", "unity", "judgment", "canadian", "forestry",
-                              "classroom", "rainbow", "hope", "shift", "quest", "elite", "achieve", "loyalty", "legendary", "word",
-                              "goat", "leader", "avenue", "straw", "glue", "offense", "dine", "coal", "minute", "socialist",
-                              "judicial", "gradual", "throne", "hook", "graze", "sermon", "still", "pie", "exemption", "worth",
-                              "queue", "record", "crackpot", "departure", "pawn", "corn", "publisher", "degree", "air", "crime"};
-
+    const std::string wordlist[50]={"marine", "dimension", "equinox", "robot", "infinite", "dragon", "unity", "judgment", "canadian", "forestry",
+                                    "classroom", "rainbow", "hope", "shift", "quest", "elite", "achieve", "loyalty", "legendary", "word",
+                                    "goat", "leader", "avenue", "straw", "glue", "offense", "dine", "coal", "minute", "socialist",
+                                    "judicial", "gradual", "throne", "hook", "graze", "sermon", "still", "pie", "exemption", "worth",
+                                    "queue", "record", "crackpot", "departure", "pawn", "corn", "publisher", "degree", "air", "crime"};
     bool play = true;
-    while(play){
-        string word = wordlist[random(0, 49)];
+
+    cout << "==============\n"; //Game title
+    cout << "Word Scrambler\n";
+    cout << "==============\n";
+
+    while(play){ //gameplay loop
+        const string word = wordlist[random(0, 49)];
         string next, answer;
         bool isCorrect = false;
+        int counter = 1;
         string scrambled = scramble(scramble(scramble(scramble(word))));
-        cout << "Unscramble the following word: ";
-        cout << scrambled << "\n";
+        cout << "Unscramble the following word: " << scrambled << "\nYou have 3 guesses.\n";
         cin >> answer;
         while(!isCorrect){
             if(answer == word){
@@ -44,14 +48,20 @@ int main(){
                 isCorrect = true;
             }
             else{
-                cout << "That's incorrect. Try again!\n";
-                cin >> answer;
+                if(counter == 3){
+                    cout << "That's incorrect. The unscrambled word was " << word << ".\n";
+                    isCorrect = true;
+                }
+                else{
+                    cout << "That's incorrect. Try again!\n";
+                    cout << "You have " << 3 - counter << " guesses left.\n";
+                    counter++;
+                    cin >> answer;
+                }                
             }
-        }
-        
+        }       
         cout << "Play again? Type yes to continue.\n";
         cin >> next;
         if(next != "yes") play = false;
-    }
-    
+    }    
 }
